e5937b18d1571db553e8b3a0794d6ac6e20e565a
Marguerite Choyer <margueritechoyer@gmail.com>
2022-08-16 12:47:32 +0200

    Modify completed value

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "tasks", to: "tasks#index"
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "articles#index"
+
+  # READ
+  get "index", to: "tasks#index"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
