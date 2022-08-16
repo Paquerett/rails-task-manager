a77fe4019b1bce7dfabb49d606014ec3dd4a0d0b
Marguerite Choyer <margueritechoyer@gmail.com>
2022-08-16 16:35:29 +0200

    Tout pas beau

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,18 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "articles#index"
 
+  #CREATE
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+
   # READ
   get "tasks", to: "tasks#index"
   get "tasks/:id", to: "tasks#show", as: :task
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # UPDATE
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # DELETE
+  delete "tasks/:id", to: "tasks#destroy"
 end
